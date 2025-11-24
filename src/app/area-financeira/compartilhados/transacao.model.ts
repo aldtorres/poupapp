288025475c1fdd5f4593ed8292454f0b39a10696
torrestor <torres.jovem@gmail.com>
2025-11-24 19:11:55 -0300

    transação

--- src/app/area-financeira/compartilhados/transacao.model.ts
+++ src/app/area-financeira/compartilhados/transacao.model.ts
@@ -2,14 +2,21 @@ import { nanoid } from "nanoid";
 
 export class Transacao {
   public readonly id = nanoid();
+  public readonly data: Date;
 
   constructor(
     public readonly nome: string,
     public readonly tipo: TipoTransacao,
     public readonly valor: number,
-    public readonly data: Date,
+    dataTransacao: string,
     public readonly conta: string
-  ) {}
+  ) {
+    if (!dataTransacao.includes('T') || !dataTransacao.includes(' ')){
+      dataTransacao += "T00:00";
+    }
+
+    this.data = new Date(dataTransacao);
+  }
 }
 
 export enum TipoTransacao {
--- src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
+++ src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
@@ -2,7 +2,7 @@ import { Component, effect, signal } from '@angular/core';
 import { BotaoComponent } from "../../../compartilhados/botao/botao.component";
 import { ModalComponent } from "../../../compartilhados/modal/modal.component";
 import { FormsModule } from "@angular/forms"
-import { TipoTransacao } from '../../compartilhados/transacao.model';
+import { TipoTransacao, Transacao } from '../../compartilhados/transacao.model';
 import { KeyValuePipe } from '@angular/common';
 
 @Component({
@@ -41,6 +41,16 @@ export class BotaoAdicionarTransacaoComponent {
   aoSubmeter(){
     console.log('enviado formulario....');
     console.log(this.novaTransacaoForm);
+    const novaTransacao = new Transacao(
+      this.novaTransacaoForm.nome, 
+      this.novaTransacaoForm.tipo as TipoTransacao, 
+      Number(this.novaTransacaoForm.valor) , 
+      this.novaTransacaoForm.data, 
+      this.novaTransacaoForm.conta
+    )
+    
+    console.log('cconvertido....');
+    console.log(novaTransacao);
   }
 
 }
