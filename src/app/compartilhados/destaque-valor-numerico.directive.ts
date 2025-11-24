e141f3a01c390656606d5e90948f5306b6b7bfc8
torrestor <torres.jovem@gmail.com>
2025-11-24 16:20:34 -0300

    DIRETIVA - appDestaqueValorNumericoDirective - passando valor para verificar como condição

--- src/app/compartilhados/destaque-valor-numerico.directive.ts
+++ src/app/compartilhados/destaque-valor-numerico.directive.ts
@@ -1,4 +1,4 @@
-import { afterRender, Directive, ElementRef } from "@angular/core";
+import { afterRender, Directive, ElementRef, input } from "@angular/core";
 
 //somente para atributos personalizados
 //DECORATOR
@@ -7,14 +7,24 @@ import { afterRender, Directive, ElementRef } from "@angular/core";
 })
 export class DestaqueValorNumericoDirective {
 
+    appDestaqueValorNumericoDirective = input.required<number>();
+
     //ElementRef ref do elemento do DOm = > iNJE.dep
     constructor(elemento : ElementRef<HTMLElement>){
         console.log('DIRETIVA --> DestaqueValorNumericoDirective APLICADO!')
         console.log(elemento)
 
         afterRender(() =>{
+            console.log('appDestaqueValorNumericoDirective: ' + this.appDestaqueValorNumericoDirective());
             //sempre dentro do afterRender, alterar apenas após renderizar!
-            elemento.nativeElement.style.color = "var(--destaque-receita)";
+            
+            if(this.appDestaqueValorNumericoDirective() > 0){
+                elemento.nativeElement.style.color = "var(--destaque-receita)";
+            }else if(this.appDestaqueValorNumericoDirective() < 0){
+                elemento.nativeElement.style.color = "var(--destaque-despesa)";
+            }
+            
+
             //elemento.nativeElement.style.color = "green";
         });
         
